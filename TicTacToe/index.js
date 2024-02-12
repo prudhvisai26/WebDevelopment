f035af60da316a6b47772e8785244996ad79d9d3
Prudhvi21 <dasariprudhvisai26@gmail.com>
2024-02-12 13:00:45 +0530

    Tic Tac Toe Updated.

--- TicTacToe/index.js
+++ TicTacToe/index.js
@@ -15,13 +15,85 @@ const win = [
   [2, 4, 6],
 ];
 
-currentPlayer = x;
+let currentPlayer = x;
+let options = ["", "", "", "", "", "", "", "", ""];
+let player = "X";
+let running = false;
 init();
 
 function init() {
   boxEl.forEach((box) => box.addEventListener("click", boxClick));
+  restartEl.addEventListener("click", restartGame);
+  statusEl.textContent = `Now ${player} Turn`;
+  running = true;
+}
+
+function restartGame() {
+  options = ["", "", "", "", "", "", "", "", ""];
+  currentPlayer = x;
+  player = "X";
+  running = true;
+  statusEl.textContent = `Now "${player}" Turn`;
+  statusEl.style.color = "black";
+  restartEl.innerHTML = "Restart 🔁";
+  boxEl.forEach((box) => {
+    box.innerHTML = "";
+    box.classList.remove("win");
+  });
 }
 
 function boxClick(e) {
-  console.log(e.index);
+  const index = e.target.dataset.index;
+  if (options[index] != "" || !running) {
+    return;
+  }
+  updateBox(index, e.target);
+  checkwin();
+}
+
+function updateBox(index, box) {
+  options[index] = player;
+  box.innerHTML = currentPlayer;
+}
+
+function changePlayer() {
+  if (currentPlayer == x) {
+    currentPlayer = o;
+    player = "O";
+  } else {
+    currentPlayer = x;
+    player = "X";
+  }
+  statusEl.textContent = `Now ${player} Turn`;
+  statusEl.style.color = "black";
+}
+
+function checkwin() {
+  let isWon = false;
+  for (let i = 0; i < win.length; i++) {
+    const data = win[i];
+    const box1 = options[data[0]];
+    const box2 = options[data[1]];
+    const box3 = options[data[2]];
+    if (box1 == "" || box2 == "" || box3 == "") continue;
+    else if (box1 == box2 && box2 == box3) {
+      isWon = true;
+      boxEl[data[0]].classList.add("win");
+      boxEl[data[1]].classList.add("win");
+      boxEl[data[2]].classList.add("win");
+    }
+  }
+  if (isWon) {
+    statusEl.textContent = `Hurrah...! "${player}" Won the game🕺`;
+    statusEl.style.color = "green";
+    restartEl.textContent = "Play Again 😉";
+    running = false;
+  } else if (!options.includes("")) {
+    statusEl.innerHTML = `Oops..! Game Draw..!`;
+    statusEl.style.color = "red";
+    restartEl.textContent = "Play Again 😌";
+    running = false;
+  } else {
+    changePlayer();
+  }
 }
